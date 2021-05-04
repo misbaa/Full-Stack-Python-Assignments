e45639cdd146fd56d2b54876dd77188ddb840018
misbaa <68369962+misbaa@users.noreply.github.com>
2021-05-04 21:32:33 +0530

    Add files via upload

---
+++ findmethod.py
@@ -0,0 +1,16 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+
+
+>>> str="what we think we become we"
+>>> x=str.find("we")
+>>> print(x)
+5
+>>> y=str.find("we",6)
+>>> print(y)
+14
+>>> z=str.find("we",15)
+>>> print(z)
+24
+
+         ^
